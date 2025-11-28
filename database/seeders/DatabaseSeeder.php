8c229321fced511b83360194f70c089f9cf143ce
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-11-28 14:22:35 -0400

    Agregar seeders para las tablas 'colegios', 'configuraciones', 'cursos', 'docentes', 'estudiantes', 'materias' y 'usuarios' con métodos de ejecución vacíos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            ConfiguracionesSeeder::class,
+            UsersSeeder::class,
+            ColegiosSeeder::class,
+            CursosSeeder::class,
+            MateriasSeeder::class,
+            DocentesSeeder::class,
+            EstudiantesSeeder::class,
         ]);
     }
 }
