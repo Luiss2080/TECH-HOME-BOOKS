67cef7c2140a0ca6ac6c7015dae75095d05cbf41
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-11 13:13:15 -0400

    feat: Actualizar el favicon en las páginas de login y admin para mantener la coherencia visual

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Dashboard Admin - Tech Home Books')</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/faviconTH.png') }}">
 
     
     <!-- Precargar fuentes -->
