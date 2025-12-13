d1573659bd4352540573a3cf95ddd4479f6c85ea
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-13 14:22:41 -0400

    feat: add teacher management index page with teacher listing, search, filters, and action buttons.

--- resources/views/admin/docentes/index.blade.php
+++ resources/views/admin/docentes/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('css')
     <link rel="stylesheet" href="{{ asset('css/admin/docentes/index.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/pages/docentes.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/pages/paginacion.css') }}">
 @endsection
 
 @section('content')
